efd71fa008e4d177b4499cd899f0e43d389ba3af
Dmitry Spidchenko <spidchenko.d@gmail.com>
2022-05-21 01:03:08 +0300

    New simple unit test

--- app/src/test/java/d/spidchenko/canvasgame/ExampleUnitTest.kt
+++ app/src/test/java/d/spidchenko/canvasgame/ExampleUnitTest.kt
@@ -10,7 +10,20 @@ import org.junit.Test
  */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        Assert.assertEquals(4, (2 + 2).toLong())
+    fun uncover_covered_cell_isCorrect() {
+        GameView.canvasCenter = FloatPoint(0.0, 0.0)
+
+        val cell = Cell(q = 0, r = 0)
+        cell.state = Cell.State.COVERED
+        cell.flag()
+        Assert.assertTrue(cell.state == Cell.State.FLAGGED)
+        cell.uncover()
+        Assert.assertFalse(cell.state == Cell.State.UNCOVERED)
+        cell.flag()
+        cell.uncover()
+        Assert.assertEquals(cell.state, Cell.State.UNCOVERED)
+        cell.state = Cell.State.FLAGGED
+        cell.uncover()
+        Assert.assertTrue(cell.state == Cell.State.FLAGGED)
     }
 }
